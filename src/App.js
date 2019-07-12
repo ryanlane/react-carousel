f0c7b9809e2806a4a6b7df5671cbe2c8a30ad625
Ryan Lane <futile@gmail.com>
2019-07-11 21:01:03 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,48 @@ import React from 'react';
 import { Carousel } from './components';
 import './App.css';
 
+const sampleCollection = [
+  {
+    id: 0,
+    title: 'title 0',
+    imageName: 'image.png'
+  },
+  {
+    id: 1,
+    title: 'title 1',
+    imageName: 'image.png'
+  },
+  {
+    id: 2,
+    title: 'title 2',
+    imageName: 'image.png'
+  },
+  {
+    id: 3,
+    title: 'title 3',
+    imageName: 'image.png'
+  },
+  {
+    id: 4,
+    title: 'title 4',
+    imageName: 'image.png'
+  },
+  {
+    id: 5,
+    title: 'title 5',
+    imageName: 'image.png'
+  },
+  {
+    id: 6,
+    title: 'title 6',
+    imageName: 'image.png'
+  }
+];
+
 function App() {
   return (
     <div className="App">
-      <Carousel />
+      <Carousel collection={sampleCollection} />
     </div>
   );
 }
