3ac1c0085d9b1ea6b7cb3455f874440ddf306adb
Ryan Lane <futile@gmail.com>
2019-07-11 20:09:31 -0700

    Create index.js

---
+++ src/components/Carousel/index.js
@@ -0,0 +1,5 @@
+/* We really only need to export the Carousel component, but 
+if we ever wanted to use one of the other sub components now you can */
+export { default as Carousel } from './Carousel';
+export { default as CarouselItem } from './CarouselItem';
+export { default as CarouselDetails } from './CarouselDetails';
