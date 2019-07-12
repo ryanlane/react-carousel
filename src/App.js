fd2133498d99c29831f96266aad73b52c5d0c942
Ryan Lane <futile@gmail.com>
2019-07-11 21:21:11 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,48 +1,13 @@
 import React from 'react';
 import { Carousel } from './components';
-import './App.css';
+import { sampleCollection } from './data/sampledata';
+import styles from './App.module.scss';
 
-const sampleCollection = [
-  {
-    id: 0,
-    title: 'title 0',
-    imageName: 'image.png'
-  },
-  {
-    id: 1,
-    title: 'title 1',
-    imageName: 'image.png'
-  },
-  {
-    id: 2,
-    title: 'title 2',
-    imageName: 'image.png'
-  },
-  {
-    id: 3,
-    title: 'title 3',
-    imageName: 'image.png'
-  },
-  {
-    id: 4,
-    title: 'title 4',
-    imageName: 'image.png'
-  },
-  {
-    id: 5,
-    title: 'title 5',
-    imageName: 'image.png'
-  },
-  {
-    id: 6,
-    title: 'title 6',
-    imageName: 'image.png'
-  }
-];
+
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <Carousel collection={sampleCollection} />
     </div>
   );
